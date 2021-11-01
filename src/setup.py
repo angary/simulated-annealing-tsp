b765d7a0d1fddedf3abfd4d3694f7f7f3600c578
Gary Sun <garysun0451@gmail.com>
2021-11-02 00:56:35 +1100

    Fix bug with scaling negative coordinates, and ensure it is scaled the same in x and y

--- src/setup.py
+++ src/setup.py
@@ -7,10 +7,10 @@ def get_random_cities(height: int, width: int, n: int) -> List[Tuple[int, int]]:
     Generate a series of random coordinates within the bounds of the
     height and width
 
-    :param height: the maximum height
-    :param width: the maximum width
-    :param n: the number of coordinates
-    :return: a list of city coordinates
+    @param height: the maximum height
+    @param width: the maximum width
+    @param n: the number of coordinates
+    @return: a list of city coordinates
     """
     cities = []
     for _ in range(n):
@@ -22,16 +22,19 @@ def load_cities(filepath: str) -> List[Tuple[float, float]]:
     """
     Load the cities in from a file, returning a list of the cities coordinates
 
-    :param filepath: the filepath to the file containing the tsp data
-    :return: a list of the city coordinates
+    @param filepath: the filepath to the file containing the tsp data
+    @return: a list of the city coordinates
     """
     cities = []
     with open(filepath, "r") as f:
         lines = f.readlines()
-        start_idx = lines.index("NODE_COORD_SECTION\n")
-        lines = lines[start_idx + 1:-1]
+        start = lines.index("NODE_COORD_SECTION\n")
+        end = lines.index("EOF\n")
+        lines = lines[start + 1:end]
         for line in lines:
-            cities.append(list(map(float, line.split()[1:])))
+            coords = list(map(float, line.split()[1:]))
+            coords.reverse()
+            cities.append(coords)
     return cities
 
 
@@ -42,24 +45,33 @@ def normalise_coords(cities: List[Tuple[float, float]], height: int, width: int)
     within the height and width. Used for ensuring that the city
     coordinates can be visualised on a screen.
 
-    :param cities: the list of city coordinates
-    :param height: the maximum height
-    :param width: the maximum width
-    :return: a list of normalised city coordinates
+    @param cities: the list of city coordinates
+    @param height: the maximum height
+    @param width: the maximum width
+    @return: a list of normalised city coordinates
     """
     xs = [c[0] for c in cities]
     ys = [c[1] for c in cities]
+
+    # Find minimum and maximum values
     min_x, max_x = min(xs), max(xs)
     min_y, max_y = min(ys), max(ys)
+
+    # Convert minimum values to positive if needed
+    min_x = max(-min_x, 0)
+    min_y = max(-min_y, 0)
 
     # Find scaling factor
     scale_x = (width - 1) / (max_x + min_x)
     scale_y = (height - 1) / (max_y + min_y)
+
+    # Choose the smaller scale to prevent overflow
+    scale = min(scale_x, scale_y)
+
     normalised = []
     for c in cities:
-
         # Ensure that all the cities are properly scaled
-        normalised_x = int(scale_x * (c[0] + min_x))
-        normalised_y = height - int(scale_y * (c[1] + min_y))
+        normalised_x = int(scale * (c[0] + min_x))
+        normalised_y = height - int(scale * (c[1] + min_y))
         normalised.append((normalised_x, normalised_y))
     return normalised
